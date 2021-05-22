e7042c92e33dcb9c94db0ec3671cc6f441b37db5
nejishow <ashrafmuhamad2014@gmail.com>
2021-05-22 12:44:13 +0300

    modified start

--- start.sh
+++ start.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-/usr/bin/java -jar -Dserver.port=8089 game-0.0.1-SNAPSHOT.jar
-sed -i -e 's/$PORT/'"$PORT"'/g' /etc/nginx/conf.d/default.conf && nginx -g 'daemon off;'
+/usr/bin/java -jar -Dserver.port=8089 game-0.0.1-SNAPSHOT.jar ; sed -i -e 's/$PORT/'"$PORT"'/g' /etc/nginx/conf.d/default.conf && nginx -g 'daemon off;'
